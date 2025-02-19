3742596f644be6e5b87568e642af631c20eebe07
ygyubyv bc2024-3 <vasys004@gmail.com>
2025-02-19 15:40:07 +0200

    Finished lab-8 tasks, added select for filtering by rating & made web-app responsive. Starting developing for lab-9

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,9 +3,12 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ movies }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [minRating, setMinRating] = useState(0);
 
-  const filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.title.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      movie.rating >= minRating
   );
 
   return (
@@ -16,6 +19,14 @@ const MovieList = ({ movies }) => {
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+      <select value={minRating} onChange={(e) => setMinRating(e.target.value)}>
+        <option value={0}>Всі рейтинги</option>
+        <option value={5}>5 і вище</option>
+        <option value={6}>6 і вище</option>
+        <option value={7}>7 і вище</option>
+        <option value={8}>8 і вище</option>
+        <option value={9}>9 і вище</option>
+      </select>
       <div className="movie-list">
         {filteredMovies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
